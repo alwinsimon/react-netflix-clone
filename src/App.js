5966cd0a6ede46a7fa0f105b915f5c6cd90ac076
Alwin Simon <003alwin@gmail.com>
2023-08-14 16:18:19 +0530

    Configured posts row with dynamic data

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import NavBar from './Components/NavBar/NavBar';
 import Banner from './Components/Banner/Banner';
 import RowPost from './Components/RowPost/RowPost';
 
+import { TMDB_MOVIES_SUGGESTION_URL, TMDB_MOVIES_ACTION_URL } from './constants/constants';
+
 
 function App() {
 
@@ -17,7 +19,9 @@ function App() {
 
       <Banner/>
 
-      <RowPost/>
+      <RowPost title="Netflix Originals" url={TMDB_MOVIES_SUGGESTION_URL} />
+
+      <RowPost title="Netflix Action" url={TMDB_MOVIES_ACTION_URL} isSmall />
 
     </div>
 
